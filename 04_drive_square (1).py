5b05db04ead26f6fa8aae4d135ed98b9d6e310a7
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2020-11-24 21:09:02 -0500

    Delete 04_drive_square (1).py

--- 04_drive_square (1).py
+++
@@ -1,8 +0,0 @@
-import cozmo
-from cozmo.util import degrees, distance_mm, speed_mmps
-def cozmo_program(robot: cozmo.robot.Robot):
-    for _ in range(8):
-        robot.drive_straight(distance_mm(200), speed_mmps(50)).wait_for_completed()
-        robot.turn_in_place(degrees(90), speed=degrees(50)).wait_for_completed()
-cozmo.run_program(cozmo_program)
-# robot.turn_in_place(degrees(90), speed=degrees(50)).wait_for_completed()
